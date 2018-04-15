bf161b9e598f884f9c23b16c8e366d694d2a8a21
demoncey <demoncey@gmail.com>
2018-04-15 18:36:22 +0200

    door and walls added

---
+++ BuildingEscape/Source/BuildingEscape/DoorComponent.cpp
@@ -0,0 +1,34 @@
+// Copyrights ppi
+
+#include "DoorComponent.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UDoorComponent::UDoorComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	AActor *owner = GetOwner();
+	FRotator newRotation = FRotator(0.0f, 120.0f, 0.0f);
+	owner->SetActorRotation(newRotation);
+}
+
+
+// Called every frame
+void UDoorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ BuildingEscape/Source/BuildingEscape/DoorComponent.h
@@ -0,0 +1,29 @@
+// Copyrights ppi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DoorComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UDoorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
